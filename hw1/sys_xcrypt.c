3e8ac1668896ba4c601328482aa3f5dfd167d9fb
Naman Mittal <nmittal@cs.stonybrook.edu>
2015-09-24 23:38:54 -0400

    Arguments copying code fron user to kernel space

--- hw1/sys_xcrypt.c
+++ hw1/sys_xcrypt.c
@@ -1,13 +1,14 @@
 #include <linux/linkage.h>
 #include <linux/moduleloader.h>
-
+#include <linux/uaccess.h>
+#include <linux/slab.h>
 asmlinkage extern long (*sysptr)(void *arg);
 
 ///////////TYPEDEF for saving user provided inputs////////////
  struct input_data{
-	char input_file[50];
-	char output_file[50];
-	char keybuf[50];
+	char* input_file;
+	char* output_file;
+	char* keybuf;
 	int keylen;
 	int flags;
 }((__attribute_packed__));
@@ -15,7 +16,22 @@ asmlinkage extern long (*sysptr)(void *arg);
 
 asmlinkage long xcrypt(void *arg)
 {	/******VARIABLE DECLARATIONS**********/
-	struct input_data* point = (struct input_data*)arg;
+	struct input_data* point = kmalloc(sizeof(struct input_data),__GFP_WAIT);
+	if(point==NULL)
+		return -EFAULT;
+	if(copy_from_user(point,(struct input_data*)arg,sizeof(struct input_data)))
+		return -EFAULT;
+	point->input_file = kmalloc(strlen_user(((struct input_data*)arg)->input_file)+1,__GFP_WAIT);
+	if(copy_from_user(point->input_file,((struct input_data*)arg)->input_file,strlen_user(((struct input_data*)arg)->input_file)+1))
+		return -EFAULT;
+	point->output_file = kmalloc(strlen_user(((struct input_data*)arg)->output_file)+1,__GFP_WAIT);
+	if(copy_from_user(point->output_file,((struct input_data*)arg)->output_file,strlen_user(((struct input_data*)arg)->output_file)+1))
+                return -EFAULT;
+	point->keybuf = kmalloc(strlen_user(((struct input_data*)arg)->keybuf)+1,__GFP_WAIT);
+	if(copy_from_user(point->keybuf,((struct input_data*)arg)->keybuf,strlen_user(((struct input_data*)arg)->keybuf)+1))
+		return -EFAULT;
+
+
 	//point->input_file = arg->input_file;
 	//char* input_file = (char*)ptr[1];
 /*	char* output_file = (char*)(ptr[2]);
