46a65f7cdd466dc85fa39e18005b35575faf7e64
Naman Mittal <nmittal@cs.stonybrook.edu>
2015-11-29 21:49:46 -0500

    Mutex in consumer and submitjob

--- hw3/sys_submitjob.c
+++ hw3/sys_submitjob.c
@@ -11,6 +11,7 @@
 #include <linux/namei.h>
 #include <linux/kthread.h>
 #include <linux/semaphore.h>
+#include <linux/mutex.h>
 #include "job_queue.h"
 #define MAX 128
 
@@ -22,9 +23,14 @@ asmlinkage extern long (*sysptr)(void *arg, int argslen);
 struct job_queue *jobs = NULL;
 struct job_queue *job = NULL;
 int flag = 0; /* This flag is used to kill the thread */
+int count = 0;
+
+static DEFINE_MUTEX(lock);
+
 static struct task_struct *consumer = NULL;
 static wait_queue_head_t waitqueue_consumer;
 int condition = 0;
+
 asmlinkage long submitjob(void *arg, int argslen)
 {	
 	int counter = 0;
@@ -35,12 +41,17 @@ asmlinkage long submitjob(void *arg, int argslen)
 	list_for_each_entry(next_job, &jobs->job_q, job_q) {
 		counter = counter + 1;
 	}
-	printk("Value of Counter %d\n,",&counter);
-	if (counter >= MAX) {
+	printk("Value of Counter %d\n,",counter);
+	mutex_lock(&lock);
+	if (count >= MAX) {
+		mutex_unlock(&lock);
 		printk("Error: Queue is full\n");
 		error = -EAGAIN;
 		goto out;
+	} else {
+		mutex_unlock(&lock);
 	}
+
 	job = NULL;
 	if (!access_ok(VERIFY_READ, arg, sizeof(struct job_metadata))) {
 		error = -EACCES;
@@ -137,14 +148,19 @@ asmlinkage long submitjob(void *arg, int argslen)
 			printk("In default\n");
 			break;
 	}
+	mutex_lock(&lock);
 	list_add_tail(&job->job_q, &(jobs->job_q));
-	if (!counter) {
+	
+	if (!count) {
 		printk("Checked condition of counter waking consumer q up\n");
 
 		condition = 1;
 		wake_up_interruptible(&waitqueue_consumer);
 	}
-
+	/* This count keeps the number of jobs in the queue */
+	/* Here only relinquish the lock on the linkedlist */
+	count++;
+	mutex_unlock(&lock);
 
 
 	goto out;
@@ -172,69 +188,65 @@ static int consume(void *data)
 	struct job_queue *get_job = NULL;
 	//struct job_metadata job_data;
 	printk("In Kernel Thread\n");
-run:
-	head = jobs;
-	//job_data = NULL;
-	list_for_each_safe(pos, q, &head->job_q) {
-		get_job = list_entry(pos, struct job_queue, job_q);
-		/* delting from list so that in MT systems no other thread work
-		 * on same data 
-		 */
-		list_del(pos);
-		switch(get_job->job_d.type) {
-		case 1:
-			printk("In Job Type 1\n");
-			//job_data = get_job->job_d;
-			xcrypt(get_job->job_d);
+	while (!flag) { /* 
+			 *  Should we add condition to
+			 * continue till all jobs are over of not?
+			 */
+		mutex_lock(&lock);
+		head = jobs;
+		//job_data = NULL;
+		list_for_each_safe(pos, q, &head->job_q) {
+			get_job = list_entry(pos, struct job_queue, job_q);
+			/* delting from list so that in MT systems no other thread work
+			* on same data 
+			*/
+			list_del(pos);
+			switch(get_job->job_d.type) {
+			case 1:
+				printk("In Job Type 1\n");
+				//job_data = get_job->job_d;
+				xcrypt(get_job->job_d);
+				break;
+			case 2:
+				printk("In Job type 2\n");
+				break;
+			case 3:
+				printk("In job type 3\n");
+				break;
+			case 4:
+				printk("In Job type 4\n");
+				break;
+			default:
+				printk("Nothing to do\n");
+			}
+			/*
+			* #Note clean the memory as per function inside the specific
+			* functions only
+			*/
+			/*
+			* Note : cleaning the memory consume by job data structure
+			*/
+			if (get_job) {
+				kfree(get_job);
+				get_job = NULL;
+			}
+			count--;
 			break;
-		case 2:
-			printk("In Job type 2\n");
-			break;
-		case 3:
-			printk("In job type 3\n");
-			break;
-		case 4:
-			printk("In Job type 4\n");
-			break;
-		default:
-			printk("Nothing to do\n");
-		}
-		/*
-		 * #Note clean the memory as per function inside the specific
-		 * functions only
-		 */
-		/*
-		 * Note : cleaning the memory consume by job data structure
-		 */
-		if (get_job) {
-			kfree(get_job);
-			get_job = NULL;
-		}
-		//Write kernel sleep/wait code here.
-		//schedule();
-		//printk ("Thread going for a sleep\n");
-		//add_wait_queue(
-		//wake_up_interruptible(&waitqueue_consumer);
-		/*
-		 * condition = 0;
-		wait_event_interruptible(waitqueue_consumer, condition == 1);
-		printk("Thread again running\n");
-		if (!flag) {
-			goto run;
-		}
-		*/
 
+		}
+		//mutex_unlock(&lock);
+		if (count == 0) {
+			condition = 0;
+			mutex_unlock(&lock);
+
+			wait_event_interruptible(waitqueue_consumer, condition == 1);
+			printk("Thread running again\n");
+		} else {
+			mutex_unlock(&lock);
+		}
 
 	}
-	
-	condition = 0;
-	wait_event_interruptible(waitqueue_consumer, condition==1);
-	printk("Thread running again\n");
-	if (!flag) {
-		goto run;
-	}
 
-	//goto run;
 	return 1;
 
 
@@ -251,6 +263,7 @@ static int __init init_sys_submitjob(void)
 		 */
 		jobs = kmalloc(sizeof(struct job_queue), __GFP_WAIT);
 		INIT_LIST_HEAD(&jobs->job_q);
+//		mutex_init(&lock, NULL, NULL);
 		consumer = kthread_create(consume, NULL, "consumer");
 		init_waitqueue_head(&waitqueue_consumer);
 		wake_up_process(consumer);
@@ -263,6 +276,7 @@ static void  __exit exit_sys_submitjob(void)
 	flag = 1;
 	condition = 1;
 	wake_up_interruptible(&waitqueue_consumer);
+	
 	if (sysptr != NULL)
 		sysptr = NULL;
 	if (consumer) {
